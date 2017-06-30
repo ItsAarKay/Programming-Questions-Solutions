fc240a1434efd6e5116bf2cfab9028ce1a4391b7
AarKay <aarkaygautam@gmail.com>
2017-06-30 18:09:01 +0530

    Create a separate Node class

---
+++ src/LinkedLists/Node.java
@@ -0,0 +1,52 @@
+package LinkedLists;
+
+/**
+ * Created by AarKay on 30-Jun-17.
+ * www.rakeshgautam.com
+ * A Simple Linked List Node class
+ */
+class Node {
+    Node next = null;
+    int data;
+
+    // Constructor to initialize a new node element
+    Node(int d) {
+        data = d;
+    }
+
+    // add new node at the end of the linked list
+    void addNode(int d) {
+        Node end = new Node(d);
+        Node n = this;
+        while (n.next != null) {
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    // delete a node by traversing through the whole list one by one
+    Node deleteNode(Node head, int d) {
+        Node n = head;
+        if (n.data == d) {
+            return head.next;
+        }
+
+        while (n.next != null) {
+            if (n.next.data == d) {
+                n.next = n.next.next;
+                return head;
+            }
+            n = n.next;
+        }
+        return head;
+    }
+
+    // display the linked list data
+    void display() {
+        Node head = this;
+        while (head != null) {
+            System.out.println(head.data);
+            head = head.next;
+        }
+    }
+}
--- src/LinkedLists/RemoveDuplicates.java
+++ src/LinkedLists/RemoveDuplicates.java
@@ -20,54 +20,14 @@ public class RemoveDuplicates {
         myNode.addNode(8);
         myNode.addNode(5);
 
-        myNode = myNode.removeDuplicates(myNode);
+        myNode = removeDuplicates(myNode);
 
-        // display the linked list data
-        while (myNode != null) {
-            System.out.println(myNode.data);
-            myNode = myNode.next;
-        }
-    }
-}
-
-class Node {
-    Node next = null;
-    int data;
-
-    // Constructor to initialize a new node element
-    Node(int d) {
-        data = d;
+        myNode.display();
     }
 
-    // add new node at the end of the linked list
-    void addNode(int d) {
-        Node end = new Node(d);
-        Node n = this;
-        while (n.next != null) {
-            n = n.next;
-        }
-        n.next = end;
-    }
-
-    // delete a node by traversing through the whole list one by one
-    private Node deleteNode(Node head, int d) {
-        Node n = head;
-        if (n.data == d) {
-            return head.next;
-        }
-
-        while (n.next != null) {
-            if (n.next.data == d) {
-                n.next = n.next.next;
-                return head;
-            }
-            n = n.next;
-        }
-        return head;
-    }
 
     // remove duplicates from the linked list using hash table
-    Node removeDuplicates(Node list) {
+    private static Node removeDuplicates(Node list) {
         HashSet<Integer> hashSet = new HashSet<>();
         Node head = list;
         while (list != null) {
